175df6f79fa34129a06bc3f30993a738a5d4f1b4
gongfuture <gongfuture@outlook.com>
2024-10-18 09:33:44 +0800

    🐞 fix(config): 修正config以符合项目

--- nonebot_plugin_ddrace/config.py
+++ nonebot_plugin_ddrace/config.py
@@ -1,74 +1,12 @@
-from nonebot import get_driver
-from nonebot.log import logger
-from pathlib import Path
 from pydantic import BaseModel
-from typing import Dict, Union
-try:
-    import ujson as json
-except ModuleNotFoundError:
-    import json
 
+class Config(BaseModel):
+    ddr_need_at: bool = False
+    ddr_command_pre_alias: str = ""
 
-class PluginConfig(BaseModel, extra="ignore"):
-    ddr_path: Path = Path(__file__).parent / "resource"
-
-
-driver = get_driver()
-ddr_config: PluginConfig = PluginConfig.model_validate(driver.config.model_dump()))
-
-
-@driver.on_startup
-async def _() -> None:
-    if not ddr_config.ddr_path.exists():
-        ddr_config.ddr_path.mkdir(parents=True, exist_ok=True)
-
-    config_json_path: Path = ddr_config.ddr_path / "config.json"
-
-    # Initial default config
-    _config: Dict[str, Dict[str, Dict[str, Union[bool, int]]]] = {
-        "morning": {
-            "morning_intime": {
-                "enable": True,
-                "early_time": 6,
-                "late_time": 12
-            },
-            "multi_get_up": {
-                "enable": False,
-                "interval": 6
-            },
-            "super_get_up": {
-                "enable": False,
-                "interval": 3
-            }
-        },
-        "night": {
-            "night_intime": {
-                "enable": True,
-                "early_time": 21,
-                "late_time": 6
-            },
-            "good_sleep": {
-                "enable": True,
-                "interval": 6
-            },
-            "deep_sleep": {
-                "enable": False,
-                "interval": 3
-            }
-        }
-    }
-
-    if not config_json_path.exists():
-        with open(config_json_path, 'w', encoding='utf-8') as f:
-            json.dump(_config, f, ensure_ascii=False, indent=4)
-
-        logger.info("Initialized the config.json of DDRace plugin")
-
-
-    new_data_path: Path = ddr_config.ddr_path / "ddr_data.json"
-
-    if not new_data_path.exists():
-        with open(new_data_path, 'w', encoding='utf-8') as f:
-            json.dump(dict(), f, ensure_ascii=False, indent=4)
-
-        logger.warning("数据文件不存在，已重新创建数据文件！")
+    # @field_validator("weather_command_priority")
+    # @classmethod
+    # def check_priority(cls, v: int) -> int:
+    #     if v >= 1:
+    #         return v
+    #     raise ValueError("weather command priority must greater than 1")
