27a8e0298cdf0bfd96b87e346dc46b944b75e956
gongfuture <gongfuture@outlook.com>
2024-10-19 15:21:25 +0800

    🦄 refactor(commands): 从__init__.py拆分指令逻辑
    测试发现原有 #3 代码存在问题，需要修复

--- nonebot_plugin_ddrace/__init__.py
+++ nonebot_plugin_ddrace/__init__.py
@@ -17,11 +17,12 @@ from . import _version
 
 require("nonebot_plugin_htmlrender")
 require("nonebot_plugin_saa")
+require("nonebot_plugin_localstore")
 
 from nonebot_plugin_saa import MessageFactory, MessageSegmentFactory, Image, Text
 
-driver = get_driver()
-plugin_config = get_plugin_config(PluginConfig)
+from .commands import *
+
 
 #region metadata
 __version__ = _version.__version__
@@ -44,38 +45,4 @@ __plugin_meta__ = PluginMetadata(
 )
 #endregion
 
-def check_empty_arg_rule(arg: BaseMessage = CommandArg()):
-    return not arg.extract_plain_text()
-
-def trigger_rule():
-    rule = Rule()
-    if plugin_config.ddr_need_at:
-        rule = rule & to_me()
-    return rule
-
-
-points = on_command("point",aliases={"points","查分","rank","ranks","分数"}, rule=trigger_rule(), priority=13, block=True)
-# test = on_command("test", priority=13,
-#                 #   rule=trigger_rule(),
-#                   block=True)
-
-# @test.handle()
-# @test.got("args", prompt="请提供查询参数")
-# async def test_handle(bot: ConsoleBot, event: BaseEvent,args: T_State):
-#     logger.debug(f"test_handle: {args}")
-#     await test.finish()
-
-
-@points.handle()
-async def points_handle(bot: BaseBot, event: Union[V12MessageEvent, V11MessageEvent], args: BaseMessage = CommandArg()):
-    if name := args.extract_plain_text():
-        html = await result_page("player", name)
-        logger.debug(f"points_handle: {html}")
-        if "404error" in html:
-            await Text(f"未找到 {name} 的成绩信息如下：").send(at_sender=True, reply=True)
-            await points.reject()
-        pic = await html2pic(html,True,filter_css="static/player_global_ranks.css")
-        message = Text(f" {name} 的成绩") + Image(pic)
-        await message.send(at_sender=True, reply=True)
-        await points.finish()
 
