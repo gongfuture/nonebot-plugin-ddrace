555edfd8c0cbc4321fdf2640192dd5fb002a5c36
gongfuture <gongfuture@outlook.com>
2024-10-19 15:25:46 +0800

    ✨ feat(triggers): 常用bot参数

--- nonebot_plugin_ddrace/utils/__init__.py
+++ nonebot_plugin_ddrace/utils/__init__.py
@@ -1,8 +1,10 @@
 from .constants import *
 from .tools import PathClass, InputCheck
+from .triggers import Trigger
 
 __all__ = [
     "constants",
     "PathClass",
-    "InputCheck"
+    "InputCheck",
+    "Trigger"
 ]
