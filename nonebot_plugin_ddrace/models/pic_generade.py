43b04a49286e9301d1901deab6ed6b373f72cc09
gongfuture <gongfuture@outlook.com>
2024-10-21 12:10:35 +0800

    ✨ feat(pic_generade): 新增插件支持脚注

--- nonebot_plugin_ddrace/models/pic_generade.py
+++ nonebot_plugin_ddrace/models/pic_generade.py
@@ -1,29 +1,27 @@
 from pathlib import Path
-from typing import List, Optional, Union
+from typing import List, Union
 from bs4 import BeautifulSoup
-from nonebot import require,logger
+from nonebot import require, logger
 require("nonebot_plugin_htmlrender")
-from nonebot_plugin_htmlrender import html_to_pic,md_to_pic
-from ..utils import *
+from nonebot_plugin_htmlrender import html_to_pic, md_to_pic
+from ..utils import InputCheck, PathClass
 
-
-
-def json2md(json_data: dict, title_filter: list) -> str:
+def json2md(json_data: dict, title_filter: List[str]) -> str:
     """
     将 JSON 数据转换为 Markdown 表格格式的字符串。
     
     Args:
         json_data (dict): 包含数据的 JSON 字典。
-        title_filter (list): 包含表格标题的列表，用于过滤 JSON 数据中的键。
+        title_filter (List[str]): 包含表格标题的列表，用于过滤 JSON 数据中的键。
     
     Returns:
         str: 生成的 Markdown 表格格式的字符串。
     """
-    if isinstance(json_data, dict) is False:
+    if not isinstance(json_data, dict):
         error_message = "json2md: 数据格式错误"
         logger.error(error_message)
         raise ValueError(error_message)
-    if isinstance(title_filter, list) is False:
+    if not isinstance(title_filter, list):
         error_message = "json2md: 标题格式错误"
         logger.error(error_message)
         raise ValueError(error_message)
@@ -43,32 +41,41 @@ def json2md(json_data: dict, title_filter: list) -> str:
 
     return md_str
 
-
 async def md2pic(md_str: str) -> Union[bytes, str]:
     """
-    将 Markdown 字符串转换为图片字节流。
-    
+    将 Markdown 字符串转换为图片。
+
     Args:
-        md_str (str): 要转换的 Markdown 字符串。
+        md_str (str): Markdown 格式的字符串。
 
     Returns:
-        Union[bytes, str]: 生成的图片字节流或错误信息。
+        Union[bytes, str]: 生成的图片的二进制数据，如果失败则返回错误信息。
     """
-    if isinstance(await md_to_pic(md_str), bytes) is False:
-        error_message = "md2pic: 图片生成失败"
-        logger.error(error_message)
-        raise ValueError(error_message)
-    else:
-        logger.trace("md2pic: success")
+    try:
         return await md_to_pic(md_str)
+    except Exception as e:
+        error_message = f"md2pic: 图片生成失败: {e}"
+        logger.error(error_message)
+        return error_message
 
+def add_css(html_str: str, css_content: str = "", css_path: Union[Path, str] = "") -> str:
+    """
+    为 HTML 字符串添加 CSS 样式。
 
-def add_css(html_str: str, css_content: str = "", css_path: Union[Path,str] = "") -> str:
-    if InputCheck().check_type_empty(html_str, str):
-        if InputCheck().check_type(css_path, str):
+    Args:
+        html_str (str): HTML 格式的字符串。
+        css_content (str, optional): CSS 样式内容。默认为空字符串。
+        css_path (Union[Path, str], optional): CSS 文件路径。默认为空字符串。
+
+    Returns:
+        str: 添加 CSS 样式后的 HTML 字符串。
+    """
+    input_check = InputCheck()
+    if input_check.check_type_empty(html_str, str):
+        if input_check.check_type(css_path, str):
             css_path = Path(css_path)
-        css_content_exists = InputCheck().check_type_empty(css_content, str)
-        css_path_exists = InputCheck().check_type_empty(css_path, Path)
+        css_content_exists = input_check.check_type_empty(css_content, str)
+        css_path_exists = input_check.check_type_empty(css_path, Path)
         if css_content_exists and css_path_exists:
             logger.warning("add_css: 同时指定 CSS 内容和路径, 优先使用路径")
             css_content_exists = False
@@ -76,8 +83,12 @@ def add_css(html_str: str, css_content: str = "", css_path: Union[Path,str] = ""
             css_content = ""
             if css_path_exists:
                 logger.trace(f"add_css: path: {css_path}")
-                with open(css_path, 'r', encoding='utf-8') as css_file:
-                    css_content = css_file.read()
+                try:
+                    with open(css_path, 'r', encoding='utf-8') as css_file:
+                        css_content = css_file.read()
+                except Exception as e:
+                    logger.error(f"add_css: 读取 CSS 文件失败: {e}")
+                    return html_str
             if css_content_exists:
                 logger.trace("add_css: content")
                 css_content += css_content
@@ -100,12 +111,53 @@ def add_css(html_str: str, css_content: str = "", css_path: Union[Path,str] = ""
     logger.error("add_css: HTML 解析失败")
     return html_str
 
+def add_footer(html_str: str) -> str:
+    """
+    将 section 下的 p 元素使用 div 包裹起来，并在 div 中原有 p 元素之前添加一个 <p>test</p> 元素。
+
+    Args:
+        html_str (str): 包含 HTML 内容的字符串。
+
+    Returns:
+        str: 修改后的 HTML 字符串。
+    """
+    soup = BeautifulSoup(html_str, 'html.parser')
+
+    # 查找所有 section 元素
+    section = soup.select_one('body > article > section')
+
+    if section:
+        # 查找 section 下的所有直接子元素 p
+        p_elements = section.find_all('p', recursive=False)
+
+        if p_elements:
+            # 创建新的 div 元素
+            new_div = soup.new_tag('div')
+            new_div['style'] = 'display: flex; justify-content: space-around;'
+
+            # 创建新的 <p>Support by nonebot-plugin-ddrace</p> 元素
+            new_p = soup.new_tag('p')
+            new_p['style'] = 'padding-left: 1em;'
+            new_p.string = 'Support by nonebot-plugin-ddrace'
+
+            # 将新的 <p>Support by nonebot-plugin-ddrace</p> 元素添加到新的 div 中
+            new_div.append(new_p)
+
+            # 将所有原有 p 元素添加到新的 div 中
+            for p in p_elements:
+                new_div.append(p.extract())
+
+            # 将新的 div 元素插入到 section 中
+            section.append(new_div)
+
+    return str(soup)
+
 
 async def html2pic(
     html_str: str,
     dark_mode: bool = False,
-    filter_css: str = "",    
-    ) -> Union[bytes, str]:
+    filter_css: str = "",
+) -> Union[bytes, str]:
     """
     将 HTML 字符串中的指定元素转换为图片。
 
@@ -115,32 +167,32 @@ async def html2pic(
         filter_css (str): 用于过滤的 CSS
 
     Returns:
-        bytes: 生成的图片的字节数据。
+        Union[bytes, str]: 生成的图片的字节数据，如果失败则返回错误信息。
     """
-    if InputCheck().check_type_empty(html_str, str):
+    input_check = InputCheck()
+    if input_check.check_type_empty(html_str, str):
         logger.trace("html2pic: HTML 解析成功")
-        html_str = add_css(html_str, css_path=(PathClass().rootpathcomplete('static/css.css')))
-        logger.trace("html2pic: Base CSS 添加成功")
-        if dark_mode:
-            html_str = add_css(html_str, css_path=PathClass().rootpathcomplete('static/css-halloween.css'))
-            logger.trace("html2pic: Dark Mode CSS 添加成功")
-        if InputCheck().check_type_empty(filter_css, str):
-            html_str = add_css(html_str, css_path=PathClass().rootpathcomplete(filter_css))
-            logger.trace("html2pic: Filter CSS 添加成功")
-
-        result = await html_to_pic(html=html_str)
-        if result is None:
-            error_message = "html2pic: 图片生成失败"
-            logger.error(error_message)
-            raise ValueError(error_message)
-        else:
+        try:
+            html_str = add_css(html_str, css_path=PathClass().rootpathcomplete('static/css.css'))
+            logger.trace("html2pic: Base CSS 添加成功")
+            if dark_mode:
+                html_str = add_css(html_str, css_path=PathClass().rootpathcomplete('static/css-halloween.css'))
+                logger.trace("html2pic: Dark Mode CSS 添加成功")
+            if input_check.check_type_empty(filter_css, str):
+                html_str = add_css(html_str, css_path=PathClass().rootpathcomplete(filter_css))
+                logger.trace("html2pic: Filter CSS 添加成功")
+            html_str = add_footer(html_str)
+            result = await html_to_pic(html=html_str)
+            if result is None:
+                error_message = "html2pic: 图片生成失败"
+                logger.error(error_message)
+                return error_message
             return result
-    elif isinstance(html_str, dict):
+        except Exception as e:
+            error_message = f"html2pic: 图片生成过程中发生错误: {e}"
+            logger.error(error_message)
+            return error_message
+    else:
         error_message = "html2pic: HTML 解析失败"
         logger.error(error_message)
-        raise ValueError(error_message)
-    else:
-        error_message = "html2pic: 未知错误"
-        logger.error(error_message)
-        raise ValueError(error_message)
-
+        return error_message
